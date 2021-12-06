6447b0187faec835ed43de5362ff1bf8dc4cf3d3
Dewalla <95588974+Dewalla@users.noreply.github.com>
2021-12-06 16:37:04 +0700

    Update kenalan.py

--- userbot/modules/kenalan.py
+++ userbot/modules/kenalan.py
@@ -6,11 +6,11 @@ from userbot.events import register
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("`Hai Perkenalkan Namaku Ramadhani`")
+    await typew.edit("`Hai Perkenalkan Namaku Dewala`")
     sleep(3)
-    await typew.edit("20 Tahun`")
+    await typew.edit("22 Tahun`")
     sleep(1)
-    await typew.edit("`Tinggal Di tangerang, Salam Kenal:)`")
+    await typew.edit("`Tinggal Di pandeglang, Salam Kenal:)`")
 # Create by myself @localheart
 
 
@@ -22,7 +22,7 @@ async def typewriter(typew):
     sleep(3)
     await typew.edit("`Aku Sayang Kamu`")
     sleep(1)
-    await typew.edit("`I LOVE YOU 💞`")
+    await typew.edit("`I LOVE YOU EEMMMUUUAAACCCCHHH 💞`")
 # Create by myself @localheart
 
 
